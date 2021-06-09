147ee06e72f084ddf171d4ae75c97cb9cb32954b
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-06-09 23:42:44 +0300

    next pwa add

--- next.config.js
+++ next.config.js
@@ -2,10 +2,33 @@ require('dotenv').config()
 
 module.exports = {
   future: {
-    webpack5: true,
+    webpack5: true
   },
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
-      process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
-  },
+      process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN
+  }
 }
+
+const withPWA = require('next-pwa')
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+const nextConfig = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  return {
+    env: {
+      API_URL: isDev ? 'http://localhost:3000' : 'https://sibelurun.vercel.app'
+    },
+    images: {
+      domains: ['dl.airtable.com', 'www.datocms-assets.com']
+    },
+    pwa: {
+      dest: 'public',
+      disable: isDev
+    }
+  }
+}
+
+module.exports = (phase) => withPWA(nextConfig(phase))
