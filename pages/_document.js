745a0d923b2097975047bff19eed8a0fe359e202
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-06-10 00:02:02 +0300

    pwa file mnfst

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,33 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        {/* pwa */}
+        <link href="/static/icons/site.webmanifest" rel="manifest" />
+        <link
+          href="/static/icons/icon-apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/static/icons/icon-favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/icons/icon-favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          color="#ffffff"
+          href="/static/icons/icon-safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <meta content="#ffffff" name="theme-color" />
+        <meta content="#ffffff" name="msapplication-TileColor" />
+
         <body>
           <Main />
           <NextScript />
