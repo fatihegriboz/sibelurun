7a1d109b1900abda0ec1bb9dab74deda2eb86e46
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-06-23 13:38:49 +0300

    ...bla

--- pages/index.js
+++ pages/index.js
@@ -88,26 +88,26 @@ export default function Index({ subscription, airtabledata, airtabledataOS }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(0, 3)
   const metaTags = blog.seo.concat(site.favicon)
-  if (typeof window !== 'undefined') {
-    const responsive = {
-      superLargeDesktop: {
-        breakpoint: { max: 4000, min: 3000 },
-        items: 1
-      },
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 1
-      },
-      tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 1
-      },
-      mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
-      }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
   }
+
   return (
     <>
       <PageTransition>
