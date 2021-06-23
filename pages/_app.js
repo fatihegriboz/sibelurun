1c478561b511eb176aa7aa22f3afc0de206e5446
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-06-23 12:53:23 +0300

    mobile-detect remove

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,13 @@
-import App from 'next/app'
 import '../styles/index.css'
 
 import SimpleReactLightbox from 'simple-react-lightbox'
-import getPageContext from './getPageContext'
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <SimpleReactLightbox>
-//       <Component {...pageProps} />
-//     </SimpleReactLightbox>
-//   )
-// }
-class MyApp extends App {
-  constructor() {
-    super()
-    this.pageContext = getPageContext()
-  }
 
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <>
-        <SimpleReactLightbox>
-          <Component {...pageProps} />
-        </SimpleReactLightbox>
-      </>
-    )
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <SimpleReactLightbox>
+      <Component {...pageProps} />
+    </SimpleReactLightbox>
+  )
 }
 
 export default MyApp
