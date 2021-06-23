2d811a02397c81495d26c7dfd97bc9bb708d5651
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-06-23 14:58:46 +0300

    img to NextImage

--- pages/index.js
+++ pages/index.js
@@ -272,22 +272,16 @@ export default function Index({ subscription, airtabledata, airtabledataOS }) {
                 return (
                   <div key={item.Id}>
                     {item.Photo && (
-                      // <NextImage
-                      //   src={item.Photo[0].thumbnails.large.url}
-                      //   alt={item.Name}
-                      //   width={item.Photo[0].thumbnails.large.width}
-                      //   height={item.Photo[0].thumbnails.large.height}
-                      //   layout="responsive"
-                      //   objectFit="cover"
-                      //   placeholder="blur"
-                      //   blurDataURL={item.Photo[0].thumbnails.small.url}
-                      //   srl_gallery_image="true"
-                      // />
-                      <img
+                      <NextImage
                         src={item.Photo[0].thumbnails.large.url}
                         alt={item.Name}
                         width={item.Photo[0].thumbnails.large.width}
                         height={item.Photo[0].thumbnails.large.height}
+                        layout="responsive"
+                        objectFit="cover"
+                        placeholder="blur"
+                        blurDataURL={item.Photo[0].thumbnails.small.url}
+                        srl_gallery_image="true"
                       />
                     )}
                   </div>
